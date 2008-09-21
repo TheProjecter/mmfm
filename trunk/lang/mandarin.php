876f4b319d45cb4f1654c334a9661ea1d0a72033
jimmy3200 <jimmy3200@c0e35792-f94f-0410-a58d-c96b19731ac9>
2008-09-21 22:58:34 +0000

    - Fix crit on character sheet, still need to implent spell stats.. - Remove forum language files, and setup language files to support forum.

--- trunk/lang/mandarin.php
+++ trunk/lang/mandarin.php
@@ -7,6 +7,8 @@
  * Translator: ÁúÐ¥Ìì, lkou, AnDa
  */
 
+$site_encoding = "gb2312";
+
 $lang_global = array(
 	// ----- GENERAL ERROR CODES -----
 	'err_sql_conn_db' => '´íÎó - ÎÞ·¨Óë·þÎñÆ÷½¨Á¢Á¬½Ó !',
@@ -4153,4 +4155,85 @@ $lang_events = array(
       'length' => '³ÖÐøÊ±¼ä<br><small>Ìì/Ð¡Ê±',
       'events' => 'ÊÂ¼þ'
 	);
+
+$forum_lang = Array(
+
+	// BBCode you can replace this by images off course
+	"image" => "Í¼Æ¬",
+	"url" => "Á´½Ó",
+	"url2" => "Á´½ÓÃû×Ö",
+	"bold" => "´ÖÌå",
+	"italic" => "Ð±Ìå",
+	"underline" => "ÏÂ»®Ïß",
+	"code" => "´úÂë",
+	"quote" => "ÒýÓÃ",
+	"quote2" => "ÒÔÃû×ÖÒýÓÃ",
+	"wrote" => "ÊéÐ´",
+	"color" => "ÑÕÉ«",
+	"media" => "Ã½Ìå",
+	"YouTube" => "YouTube",
+
+	// forum_index
+	"forums" => "ÂÛÌ³",
+	"forum_index" => "ÂÛÌ³Ö÷Ò³",
+	"you_are_here" => "ÄãÔÚÕâÀï",
+	"last_post_by" => "×îºó¸üÐÂ£º",
+	"in" => "ÔÚ",
+	"no_topics" => "Ã»ÓÃÖ÷Ìâ...",
+	"topics" => "Topics", //todo
+
+	// view_forum
+	"no_such_forum" => "´íÎóµÄÇëÇó : Ã»ÓÃÕâ¸öÂÛÌ³",
+	"no_access" => "´íÎóµÄÇëÇó : ÄãÎÞ·¨´ò¿ªÖ¸¶¨ÄÚÈÝ",
+	"new_topic" => "ÐÂÖ÷Ìâ",
+	"annoucement" => "¹«¸æ",
+	"sticky" => "ð¤Ìù",
+	"pages" => "Ò³",
+	"title" => "Ö÷Ìâ",
+	"author" => "×÷Õß",
+	"pages" => "Ò³",
+	"replies" => "»Ø¸´",
+	"last_post" => "×îºó¸üÐÂ",
+	"closed" => "¹Ø±Õ",
+
+	// view_topic
+	"no_such_topic" => "´íÎóµÄÇëÇó : Ã»ÓÐÕâ¸öÖ÷Ìâ",
+	"info" => "ÐÅÏ¢",
+	"text" => "ÎÄ×Ö",
+	"yes" => "ÊÇ",
+	"no" => "·ñ",
+	"at" => "ÔÚ",
+	"move" => "×ªÒÆµ½",
+	"edit" => "±à¼­",
+	"delete" => "É¾³ý",
+	"post" => "·¢±í",
+	"open" => "´ò¿ª",
+	"close" => "guanhi",
+	"quick_reply" => "¿ìËÙ»Ø¸´",
+	"down" => "ÏÂ",
+	"up" => "ÉÏ",
+	"normal" => "ÆÕÍ¨",
+
+	// delete_post
+	"delete_topic" => "ÄãÕæµÄÒªÉ¾³ýÕâ¸öÖ÷ÌâºÍÏÂÃæËùÓÐµÄÐÅÏ¢Âð?",
+	"delete_post" => "ÄãÕæÒªÉ¾³ýÕâ¸öÄÚÈÝÂð?",
+	"back" => "ºóÍË",
+	"confirm" => "È·¶¨",
+
+	// add_topic
+	"topic_name" => "Ö÷ÌâÃû×Ö",
+	"please_wait" => "ÇëµÈ´ý $minfloodtime ÃëºóÔÙ·¢Ìù.",
+
+	// do_add_topic
+	"name_too_long" => "Ö÷ÌâÃûÌ«³¤ÁË!",
+	"name_too_short" => "Ö÷ÌâÃûÌ«¶ÌÁË!",
+	"msg_too_short" => "ÐÅÏ¢Ì«¶ÌÁË!",
+
+	// edit_post
+	"no_such_post" => "´íÎóµÄÇëÇó : Ã»ÓÐÕâ¸öÄÚÈÝ",
+
+	// move_topic
+	"where" => "ÄãÒª°ÑÕâ¸öÖ÷ÌâÒÆ¶¯µ½ÄÇÀï?",
+);
+
 ?>
